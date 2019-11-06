77b272a498d6c4312c69e061db41deefb05d71dc
Richard Muñoz <richard.munoz@crifa.com>
2019-11-06 09:10:56 -0500

    added hoc component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
@@ -21,6 +22,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hoc/Auxiliary.ts
@@ -0,0 +1,8 @@
+/**
+ * This is the Higher-order components layer
+ * Use it to reuse component logic as a wrapper
+ */
+
+const aux = (props: { children: any }) => props.children;
+
+export default aux;
