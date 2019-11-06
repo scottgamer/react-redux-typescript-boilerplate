2c1ed8a4088306a490d29725827bc5942c8f6453
Richard Muñoz <richard.munoz@crifa.com>
2019-11-06 12:27:32 -0500

    added fonts and cssbaseline

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { CssBaseline } from "@material-ui/core";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Body from "./Body/Body";
@@ -6,6 +7,7 @@ import Body from "./Body/Body";
 const Layout: React.FC = () => {
   return (
     <Fragment>
+      <CssBaseline />
       <Header />
       <Body />
       <Footer />
